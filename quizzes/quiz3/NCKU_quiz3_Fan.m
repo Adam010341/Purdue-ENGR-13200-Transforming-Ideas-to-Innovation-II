66289b1aa7bb795bc248e56121695ab25f749079
Adam Fan <adam010341@gmail.com>
2025-07-11 14:32:26 -0400

    Quiz 3: sum a vector with a for loop

---
+++ quizzes/quiz3/NCKU_quiz3_Fan.m
@@ -0,0 +1,20 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Quiz 3 Template
+% Semester: Summer 2024
+% Name: Adam Fan
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+format shortg
+%% Q3 | FOR LOOPS
+
+sum_x=0;
+x = [56, 23, 33, 34, 22, 21];
+for k=1:length(x)
+    sum_x=sum_x+x(k);
+end
+sum_x
+
+%% ACADEMIC INTEGRITY STATEMENT
+% I have not used source code obtained from any other unauthorized
+% source, either modified or unmodified. I have not provided
+% access to my code to anyone in any way. The script I am 
+% submitting is my own original work.
